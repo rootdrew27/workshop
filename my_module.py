97eac356eed4c0dd00e81dbb27160a07953e7695
Andrew Root <root.drew27@gmail.com>
2024-11-23 21:41:52 -0600

    minor changes

---
+++ my_module.py
@@ -0,0 +1,13 @@
+PI = 3.1456 # or something like that
+
+def myFunc1():
+    return "You called myFunc1"
+
+class Dog():
+    def __init__(self, name):
+        self.name = name
+
+    def bark(self):
+        return f'{self.name} says woof'
+    
+    
